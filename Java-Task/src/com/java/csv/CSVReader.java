75b50fdba6bac88351323c5b7abba1b79b9a107a
HanielAbraxos <102482306+HanielAbraxos@users.noreply.github.com>
2022-11-11 23:57:09 +0500

    Add files via upload

---
+++ Java-Task/src/com/java/csv/CSVReader.java
@@ -0,0 +1,14 @@
+package com.java.csv;
+
+import java.io.FileReader;
+
+public class CSVReader {
+
+    public CSVReader(FileReader fileReader, char c, char d, int i) {
+    }
+
+    public String[] readNext() {
+        return null;
+    }
+
+}
